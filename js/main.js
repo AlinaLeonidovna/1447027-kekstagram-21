68cff760e7c7a236db8204a99cb8d4e99feb426b
Alina Anisimova <anisimova.alina.leonidovna@gmail.ru>
2020-10-08 21:15:05 +0300

    Вносит правки после проверки наставником

--- js/main.js
+++ js/main.js
@@ -1,7 +1,5 @@
 'use strict';
 
-const PHOTOS_QUANTITY = 25;
-
 const Likes = {
   MIN: 15,
   MAX: 200
@@ -39,94 +37,60 @@ const NAMES = [
   `Костя`
 ];
 
-let comments = [];
-const photos = [];
+// Функция возвращает случайное число включая min и max
+const getRandomNumber = (min, max) => Math.floor(min + Math.random() * (max + 1 - min));
+
+// Функция создает список комментариев
+const createComments = (quantity) => {
+  const comments = [];
+  for (let i = 0; i < quantity; i++) {
+    const comment = {
+      avatar: `img/avatar-${getRandomNumber(AvatarsNumber.MIN, AvatarsNumber.MAX)}.svg`,
+      message: MESSAGES[getRandomNumber(0, MESSAGES.length - 1)],
+      name: NAMES[getRandomNumber(0, NAMES.length - 1)]
+    };
+    comments.push(comment);
+  }
+  return comments;
+};
+
+// Функция создает описание фотографии
+const createPhotoDescription = (quantity) => {
+  const photos = [];
+  for (let i = 0; i < quantity; i++) {
+    const photo = {
+      url: `photos/${getRandomNumber(1, 25)}.jpg`,
+      description: `описание фотографии`,
+      likes: getRandomNumber(Likes.MIN, Likes.MAX),
+      comments: createComments(getRandomNumber(CommentsQuantity.MIN, CommentsQuantity.MAX))
+    };
+    photos.push(photo);
+  }
+  return photos;
+};
+
 const photosElement = document.querySelector(`.pictures`);
 const photosTemplate = document.querySelector(`#picture`).content.querySelector(`.picture`);
 
-// Функция возвращает случайное число
-function getRandomNumber(min, max) {
-  return Math.floor(min + Math.random() * (max + 1 - min));
-}
-// const getRandomNumber = (min, max) => Math.floor(min + Math.random() * (max + 1 - min));
-
-// Функция возвращает случайный элемент массива
-function getRandomArrayElement(array) {
-  return array[Math.floor(Math.random() * array.length)];
-}
-// const getRandomArrayElement = (array) => array[Math.floor(Math.random() * array.length)];
-
-// Функция создает список комментариев
-function createComments() {
-  for (let i = 0; i < getRandomNumber(CommentsQuantity.MIN, CommentsQuantity.MAX); i++) {
-    comments[i] = [
-      {
-        avatar: `img/avatar-${getRandomNumber(AvatarsNumber.MIN, AvatarsNumber.MAX)}.svg`,
-        message: getRandomArrayElement(MESSAGES),
-        name: getRandomArrayElement(NAMES)
-      }
-    ];
-  }
-
-  return comments;
-}
-/* const createComments = (quantity) => {
-  for (let i = 0; i < quantity; i++) {
-    comments[i] = {
-      avatar: `img/avatar-${getRandomNumber(AvatarsNumber.MIN, AvatarsNumber.MAX)}.svg`,
-      message: getRandomArrayElement(MESSAGES),
-      name: getRandomArrayElement(NAMES)
-    };
-  }
-
-  return comments;
-}; */
-
-// Функция создает описание фотографии
-function createPhotoDescription(quantity) {
-  for (let i = 0; i < quantity; i++) {
-    photos[i] = {
-      url: `photos/${getRandomNumber(1, PHOTOS_QUANTITY)}.jpg`,
-      description: `описание фотографии`,
-      likes: getRandomNumber(Likes.MIN, Likes.MAX),
-      comments: createComments(getRandomNumber(CommentsQuantity.MIN, CommentsQuantity.MAX)).length
-    };
-  }
-
-  return photos;
-}
-/* const createPhotoDescription = (quantity) => {
-  for (let i = 0; i < quantity; i++) {
-    photos[i] = {
-      url: `photos/${getRandomNumber(1, PHOTOS_QUANTITY)}.jpg`,
-      description: `описание фотографии`,
-      likes: getRandomNumber(Likes.MIN, Likes.MAX),
-      comments: createComments(getRandomNumber(CommentsQuantity.MIN, CommentsQuantity.MAX)).length
-    };
-  }
-
-  return photos;
-}; */
-createPhotoDescription(PHOTOS_QUANTITY);
-
 // Функция создаст DOM-элементы, соответствующие фотографиям и заполняет их данными
-function createPhotos(photo) {
+const createPhotos = (photo) => {
   const photoElement = photosTemplate.cloneNode(true);
 
   photoElement.querySelector(`.picture__img`).src = photo.url;
   photoElement.querySelector(`.picture__likes`).textContent = photo.likes;
-  photoElement.querySelector(`.picture__comments`).textContent = photo.comments;
+  photoElement.querySelector(`.picture__comments`).textContent = photo.comments.length;
 
   return photoElement;
-}
+};
 
 // Функция отрисует сгенерированные DOM-элементы
-function createPicturesList(photo) {
+const createPicturesList = (photos) => {
   const fragment = document.createDocumentFragment();
   for (let i = 0; i < photos.length; i++) {
-    fragment.appendChild(createPhotos(photo[i]));
+    fragment.appendChild(createPhotos(photos[i]));
   }
-  photosElement.appendChild(fragment);
-}
 
-createPicturesList(photos);
+  return fragment;
+};
+
+photosElement.appendChild(createPicturesList(createPhotoDescription(25)));
